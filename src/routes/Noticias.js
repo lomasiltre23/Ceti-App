1dcac627190692944b8047088ffc1fe5f97623aa
lomasiltre23 <charlsmuvill@gmail.com>
2016-07-26 09:06:07 -0500

    Migracion a Phonegap Build
    Ya es posible utilizar phonegap build al igual que phonegap desktop para ejecutar la app pero aun hace falta seguir probandolo

--- src/routes/Noticias.js
+++ src/routes/Noticias.js
@@ -1,11 +1,8 @@
 import React, {Component} from "react"
 
-import samplePhoto from "../images/material.png";
-import sampleBig from "../images/big_sample.jpg";
-import sampleSmall from "../images/nodejs.png"
-
 import Navbar from "../components/NAVBAR/nav_bar"
 import NewsItem from "../components/NAVBAR/news_card_item"
+import icon from "../images/icon.png"
 
 class Noticias extends Component{
   constructor(){
@@ -21,17 +18,17 @@ class Noticias extends Component{
             title="Material Design"
             link="#"
             description="Descripcion para material design esta es la descricion que describe la noticia de material design."
-            photo={samplePhoto}/>
+            photo={icon}/>
           <NewsItem
             title="Photos"
             link="#"
             description="Descripcion para Photo esta es la descricion que describe la noticia de Photo."
-            photo={sampleBig}/>
+            photo={icon}/>
           <NewsItem
             title="Node JS"
             link="#"
             description="Descripcion para Node esta es la descricion que describe la noticia de Node."
-            photo={sampleSmall}/>
+            photo={icon}/>
         </div>
       </div>
     );
